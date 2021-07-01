8b806f43d6941f75eb3e4571881bd7932089f1c0
Grant Slender <gslender@gmail.com>
2021-07-01 11:39:49 +1000

    Update nue-zigbee-smart-fan-light-switch.groovy
    Fixed reporting bug whereby updates to the device were not being sent to the driver.

--- nue-zigbee-smart-fan-light-switch.groovy
+++ nue-zigbee-smart-fan-light-switch.groovy
@@ -1,5 +1,6 @@
 /**
  * =======================================================================================
+ *  Ikuu Zigbee Light Fan Controller
  *  Copyright 2021 Grant Slender
  *
  *  Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
@@ -13,13 +14,13 @@
  *
  * =======================================================================================
  *
- *  Last modified: 2021-05-09
+ *  Last modified: 2021-06-18
  *
  */ 
  
 metadata {
-   definition (name: "Nue ZigBee Smart Fan Light Switch", namespace: "gslender", author: "Grant Slender", importUrl: "https://raw.githubusercontent.com/gslender/hubitat/main/nue-zigbee-smart-fan-light-switch.groovy") {
-      
+      definition (name: "Ikuu Zigbee Light Fan Controller", namespace: "gslender", author: "Grant Slender", importUrl: "https://raw.githubusercontent.com/gslender/hubitat/main/ikuu-zigbee-light-fan-controller.groovy") {
+
       capability "Refresh"
       capability "Configuration"
        
@@ -29,12 +30,10 @@ metadata {
       command "fanMediumOn"
       command "fanHighOn"
       command "fanOff"
-      attribute "fan-high-switch", "STRING"
-      attribute "fan-medium-switch", "STRING"
-      attribute "fan-low-switch", "STRING"
+      attribute "fan", "STRING"
       attribute "light", "STRING"
 
-      fingerprint profileId: "0104", inClusters: "0000,0003,0004,0005,0006,0008,1000", outClusters: "1000", manufacturer: "3A Smart Home DE", model: "LXN56-1S27LX1.2", deviceJoinName: "Nue Smart Fan Light Switch"
+  	  fingerprint profileId: "0104", inClusters: "0000, 0004, 0005, 0006, 0003, 0202, EF00", outClusters: "0019, 000A", manufacturer: "_TZ3210_lzqq3u4r", model: "TS0501", deviceJoinName: "Ikuu Zigbee Light Fan Switch"
        
    }
       
@@ -44,40 +43,34 @@ preferences {
    }
 }
 
-private getCLUSTER_LIGHT_SWITCH() { 0x0001 }
-private getCLUSTER_FAN_HIGH_SWITCH() { 0x0002 }
-private getCLUSTER_FAN_MEDIUM_SWITCH() { 0x0003 }
-private getCLUSTER_FAN_LOW_SWITCH() { 0x0004 }
-private getCOMMAND_OFF() { 0x00 }
-private getCOMMAND_ON() { 0x01 }
 
 /* ======== driver built-in callback methods ======== */
 /* https://docs.hubitat.com/index.php?title=Device_Code */
 
 void installed(){
-   log.info "installed..."
-   device.updateSetting("enableDebug",[type:"bool", value: true])
-   device.updateSetting("enableDesc",[type:"bool", value: true])
-   initialize()
+    log.info "installed..."
+    device.updateSetting("enableDebug",[type:"bool", value: true])
+    device.updateSetting("enableDesc",[type:"bool", value: true])
+    initialize()
 }
 
 void uninstalled(){
-   log.info "uninstalled..."
+    log.info "uninstalled..."
 }
 
-void updated() {
-   log.info "updated..."
-   log.warn "debug logging is: ${enableDebug == true}"
-   log.warn "description logging is: ${enableDesc == true}"
+void updated() { 
+    log.info "updated..."
+    log.warn "debug logging is: ${enableDebug == true}"
+    log.warn "description logging is: ${enableDesc == true}"
 }
 
-void initialize() {
-   log.info "initialize..."    
-   device.setName("NUE-ZBFLB")
-   updated();
-   configure()
-   unschedule()
-   if (enableDebug) runIn(1800,logsOff)
+void initialize() {   
+    log.info "initialize..."    
+    device.setName("IKUU-FAN-LIGHT")
+    updated();
+    configure()
+    unschedule()
+    if (enableDebug) runIn(1800,logsOff)
 }
 
 void parse(String description) {
@@ -87,52 +80,56 @@ void parse(String description) {
     def now = new Date().format("yyyy MMM dd EEE h:mm:ss a", location.timeZone)
     sendEvent(name: "lastCheckin", value: now)
     if (description.startsWith("catchall")) return
-    
-    def descMap = zigbee.parseDescriptionAsMap(description) 
-    def rawValue = Integer.parseInt(descMap.value,16)
-    def value = rawValue == 1 ? "on" : "off"
-    def descriptionText
-    def name
-    
-    def ssMap = zigbee.parse(description)
-    if (ssMap.sourceEndpoint == CLUSTER_LIGHT_SWITCH) {
-        
-        name = "light"
-        getChildDeviceParse("${device.deviceNetworkId}-Light","switch",value)
-    
+
+    def event = zigbee.getEvent(description)
+    if (event) {
+      event.name = "light"
+      sendEvent(event)
+      if (enableDebug) log.debug "sendEvent( ${event} )"
+      getChildDeviceParse("${device.deviceNetworkId}-Light","switch",event.value)
     } else {
-        
-        if (ssMap.sourceEndpoint == CLUSTER_FAN_HIGH_SWITCH) {    
-            name = "fan-high-switch"       
-        }
-        if (ssMap.sourceEndpoint == CLUSTER_FAN_MEDIUM_SWITCH) {
-            name = "fan-medium-switch"
-        }
-        if (ssMap.sourceEndpoint == CLUSTER_FAN_LOW_SWITCH) {  
-            name = "fan-low-switch"
-        }
-        
-        runInMillis(100, 'calcChildFanSpeed')
+    	if (description?.startsWith("read attr -")) {
+    		def descMap = zigbee.parseDescriptionAsMap(description)
+    		if (descMap.cluster == "0202" && descMap.attrId == "0000") {
+    			event.name = "fan"
+                switch (descMap.value.toInteger()) {
+                    case 1:
+                    event.value = "low"
+                    break
+                    
+                    case 2:
+                    event.value = "medium"
+                    break
+                    
+                    case 3:
+                    case 4:
+                    event.value = "high"
+                    break
+       
+                    case 0:
+                    default :
+                    event.value = "off"                
+                }
+                sendEvent(event)
+                if (enableDebug) log.debug "sendEvent( ${event} )"
+                
+                runInMillis(100, 'calcChildFanSpeed')
+    		}
+    	}
     }
     
-    descriptionText = "${device.displayName} ${name} was turned ${value}"
-    sendEvent(name:name,value:value,descriptionText:descriptionText)
-    
-    if (enableDesc) log.info "${descriptionText}"
+    if (event) {
+        if (enableDesc) log.info "${device.displayName} ${event.name} was set to ${event.value}"
+    }
     
 }
 
 /* ======== capability commands ======== */
 
 def refresh() {
-   if (enableDebug) log.debug "refresh()"
-    
-   [
-       "he rattr 0x${device.deviceNetworkId} ${CLUSTER_LIGHT_SWITCH} 0x0006 0x0", "delay 20", // light
-       "he rattr 0x${device.deviceNetworkId} ${CLUSTER_FAN_HIGH_SWITCH} 0x0006 0x0", "delay 20", // fan-high
-       "he rattr 0x${device.deviceNetworkId} ${CLUSTER_FAN_MEDIUM_SWITCH} 0x0006 0x0", "delay 20", // fan-medium
-       "he rattr 0x${device.deviceNetworkId} ${CLUSTER_FAN_LOW_SWITCH} 0x0006 0x0", "delay 20", // fan-low
-   ]
+    if (enableDebug) log.debug "refresh()"
+   
+    return zigbee.onOffRefresh() + zigbee.readAttribute(0x0202, 0x0000)
 }
 
 
@@ -141,6 +138,7 @@ def List<String> configure() {
     
     // clean stuff up !!
     state.clear()
+    
     getChildDevices().each { 
         if (enableDebug) log.debug "deleteChildDevice ${it.deviceNetworkId}"
         deleteChildDevice("${it.deviceNetworkId}") 
@@ -148,8 +146,9 @@ def List<String> configure() {
     
     addChildType("Light","Switch")
     addChildType("Fan","Fan")
-    state.comments = "Child devices created!"
-    return
+    state.comments = "Child devices created!" 
+    return zigbee.configureReporting(0x0006, 0x0000, 0x10, 1, 600, null) +
+       zigbee.configureReporting(0x0202, 0x0000, 0x30, 1, 600, null)
 }
 
 /* ======== custom commands and methods ======== */
@@ -161,15 +160,15 @@ def calcChildFanSpeed() {
     def fanswitch = "off"
     
     
-    if (device.currentValue("fan-high-switch") == "on") {
+    if (device.currentValue("fan") == "high") {
         fanspeed = "high"
         fanswitch = "on"
     }
-    if (device.currentValue("fan-medium-switch") == "on") {
+    if (device.currentValue("fan") == "medium") {
         fanspeed = "medium"
         fanswitch = "on"
     }
-    if (device.currentValue("fan-low-switch") == "on") {
+    if (device.currentValue("fan") == "low") {
         fanspeed = "low"
         fanswitch = "on"
     }
@@ -213,43 +212,37 @@ def getChildDeviceParse(_childname,_attrib,_value) {
 def lightOn() {
     if (enableDebug) log.debug "lightOn()"
     getChildDeviceParse("${device.deviceNetworkId}-Light","switch","on")
-    sendHubCommand(new hubitat.device.HubAction("he cmd 0x${device.deviceNetworkId} ${CLUSTER_LIGHT_SWITCH} 0x0006 ${COMMAND_ON} {}", hubitat.device.Protocol.ZIGBEE))
+    sendHubCommand(new hubitat.device.HubAction("he cmd 0x${device.deviceNetworkId} 0x1 0x0006 0x1 {}", hubitat.device.Protocol.ZIGBEE))
 }
 
 def lightOff() {
     if (enableDebug) log.debug "lightOff()"
     getChildDeviceParse("${device.deviceNetworkId}-Light","switch","off")
-    sendHubCommand(new hubitat.device.HubAction("he cmd 0x${device.deviceNetworkId} ${CLUSTER_LIGHT_SWITCH} 0x0006 ${COMMAND_OFF} {}", hubitat.device.Protocol.ZIGBEE))
+    sendHubCommand(new hubitat.device.HubAction("he cmd 0x${device.deviceNetworkId} 0x1 0x0006 0x0 {}", hubitat.device.Protocol.ZIGBEE))
 }
 
 def fanOff(){
-    if (enableDebug) log.debug "fanOff()"  
-    
+    if (enableDebug) log.debug "fanOff()"      
     getChildDeviceParse("${device.deviceNetworkId}-Fan","speed","off")
-    sendHubCommand(new hubitat.device.HubAction("he cmd 0x${device.deviceNetworkId} ${CLUSTER_FAN_HIGH_SWITCH} 0x0006 ${COMMAND_OFF} {}", hubitat.device.Protocol.ZIGBEE))
-    sendHubCommand(new hubitat.device.HubAction("he cmd 0x${device.deviceNetworkId} ${CLUSTER_FAN_MEDIUM_SWITCH} 0x0006 ${COMMAND_OFF} {}", hubitat.device.Protocol.ZIGBEE))
-    sendHubCommand(new hubitat.device.HubAction("he cmd 0x${device.deviceNetworkId} ${CLUSTER_FAN_LOW_SWITCH} 0x0006 ${COMMAND_OFF} {}", hubitat.device.Protocol.ZIGBEE))
+    sendHubCommand(new hubitat.device.HubAction("he wattr 0x${device.deviceNetworkId} 0x01 0x0202 0x0000 0x30 {00}", hubitat.device.Protocol.ZIGBEE))
 }
 
 def fanHighOn(){
     if (enableDebug) log.debug "fanHighOn()"  
-//    sendEvent(name: "fan", value: "high")
     getChildDeviceParse("${device.deviceNetworkId}-Fan","speed","high")
-    sendHubCommand(new hubitat.device.HubAction("he cmd 0x${device.deviceNetworkId} ${CLUSTER_FAN_HIGH_SWITCH} 0x0006 ${COMMAND_ON} {}", hubitat.device.Protocol.ZIGBEE))
+    sendHubCommand(new hubitat.device.HubAction("he wattr 0x${device.deviceNetworkId} 0x01 0x0202 0x0000 0x30 {03}", hubitat.device.Protocol.ZIGBEE))   
 }
 
 def fanMediumOn(){
     if (enableDebug) log.debug "fanMediumOn()"
-//    sendEvent(name: "fan", value: "medium")
     getChildDeviceParse("${device.deviceNetworkId}-Fan","speed","medium")
-    sendHubCommand(new hubitat.device.HubAction("he cmd 0x${device.deviceNetworkId} ${CLUSTER_FAN_MEDIUM_SWITCH} 0x0006 ${COMMAND_ON} {}", hubitat.device.Protocol.ZIGBEE))
+    sendHubCommand(new hubitat.device.HubAction("he wattr 0x${device.deviceNetworkId} 0x01 0x0202 0x0000 0x30 {02}", hubitat.device.Protocol.ZIGBEE))
 }
 
 def fanLowOn(){
     if (enableDebug) log.debug "fanLowOn()"
-//    sendEvent(name: "fan", value: "low")
     getChildDeviceParse("${device.deviceNetworkId}-Fan","speed","low")
-    sendHubCommand(new hubitat.device.HubAction("he cmd 0x${device.deviceNetworkId} ${CLUSTER_FAN_LOW_SWITCH} 0x0006 ${COMMAND_ON} {}", hubitat.device.Protocol.ZIGBEE))
+    sendHubCommand(new hubitat.device.HubAction("he wattr 0x${device.deviceNetworkId} 0x01 0x0202 0x0000 0x30 {01}", hubitat.device.Protocol.ZIGBEE))
 }
 
 
@@ -280,7 +273,7 @@ String componentSetSpeed(cd, speed){
           fanOff()
        break
        case "on":
-          if (cd && cd.currentValue("switch") != "off") break
+          if (cd && cd.currentValue("speed") != "off") break
        case "low":
           fanLowOn()
        break
@@ -304,18 +297,18 @@ String componentCycleSpeed(cd){
     
     switch (currentSpeed) {
        case "off":
-          return componentSetLevel(cd, 33)
+          return fanLowOn()
        break
        case "low":
-          return componentSetLevel(cd, 66)
+          return fanMediumOn()
        break
        case "medium-low":
        case "medium":
        case "medium-high":
-          return componentSetLevel(cd, 99)
+          return fanHighOn()
        break
        case "high":
-          return componentOff(cd)
+          return fanOff()
        break
     }
 }
